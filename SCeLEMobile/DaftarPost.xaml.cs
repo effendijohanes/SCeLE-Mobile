bbdae2b2f99f82e975a92d1e9cbde6a7111c8e15
effendijohanes <johanes.effendi@live.com>
2014-05-11 19:00:52 +0700

    Mencoba web service forum yang baru :D

--- SCeLEMobile/DaftarPost.xaml.cs
+++ SCeLEMobile/DaftarPost.xaml.cs
@@ -7,37 +7,38 @@ using System.Windows.Controls;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
-using System.Threading.Tasks;
-using SCeLEMobile.Model;
-using System.Xml;
-using System.IO;
-using System.Xml.Serialization;
-using SCeLEMobile.Objek;
 using System.Collections.ObjectModel;
+using SCeLEMobile.Model;
+using SCeLEMobile.Objek;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
 using System.Windows.Media.Imaging;
 
 namespace SCeLEMobile
 {
-    public partial class DaftarThread : PhoneApplicationPage
+    public partial class DaftarPost : PhoneApplicationPage
     {
-        ObservableCollection<ThreadMK> mkThreads = new ObservableCollection<ThreadMK>();
+        ObservableCollection<PostMK> mkPosts = new ObservableCollection<PostMK>();
         List<string> usersID = new List<string>();
         IDictionary<string, User> users = new Dictionary<string, User>();
 
-        public DaftarThread()
+        public DaftarPost()
         {
             InitializeComponent();
-            DataContext = mkThreads;
+
         }
+
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             string id = NavigationContext.QueryString["id"];
-            getThreadsinForum(id);
+            getPostsinThread(id);
         }
 
-        private async void getThreadsinForum(string id)
+        private async void getPostsinThread(string id)
         {
-            string url = String.Format("{0}/webservice/rest/server.php?wstoken={1}&wsfunction=mod_forum_get_forum_discussions&forumids[]={2}", ParentModel.SceleURL, ParentModel.GetTokenFromIsolatedStorage(), id);
+            string url = String.Format("{0}/webservice/rest/server.php?wstoken={1}&wsfunction=mod_forum_get_forum_posts&discussionids[0]={2}", ParentModel.SceleURL, ParentModel.GetTokenFromIsolatedStorage(), id);
             Task<string> xmlTask = ParentModel.DownloadXML(new Uri(url));
             string hasil = await xmlTask;
             deserialize(hasil);
@@ -51,18 +52,17 @@ namespace SCeLEMobile
 
             foreach (SINGLE s in f.multiple.single)
             {
-                mkThreads.Add(new ThreadMK(s));
+                mkPosts.Add(new PostMK(s));
             }
 
-            foreach (ThreadMK tmk in mkThreads)
+            foreach (PostMK tmk in mkPosts)
             {
                 usersID.Add(tmk.UserID);
             }
 
             getUsers();
 
-            lstThread.ItemsSource = mkThreads;
-            lstThread.DataContext = mkThreads;
+            lstPost.ItemsSource = mkPosts;
         }
 
         private async void getUsers()
@@ -89,17 +89,15 @@ namespace SCeLEMobile
                 users[u.ID] = u;
             }
 
-            foreach (ThreadMK tmk in mkThreads)
+            foreach (PostMK tmk in mkPosts)
             {
                 tmk.Image = new BitmapImage(new Uri(users[tmk.UserID].ProfileImageURL));
             }
         }
 
-        private void list_Thread_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        private void list_Post_Tap(object sender, System.Windows.Input.GestureEventArgs e)
         {
-            ListBox lb = sender as ListBox;
-            ThreadMK tmk = lb.SelectedItem as ThreadMK;
-            NavigationService.Navigate(new Uri("/DaftarPost.xaml?id=" + tmk.ID, UriKind.Relative));
+
         }
     }
 }
---
+++ SCeLEMobile/Objek/PostMK.cs
@@ -0,0 +1,341 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace SCeLEMobile.Objek
+{
+    class PostMK : INotifyPropertyChanged
+    {
+        public PostMK(SINGLE s)
+        {
+            foreach (KEY k in s.key)
+            {
+                if (k.name.Equals("id"))
+                {
+                    ID = k.VALUE;
+                }
+                else if (k.name.Equals("discussion"))
+                {
+                    Discussion = k.VALUE;
+                }
+                else if (k.name.Equals("parent"))
+                {
+                    Parent = k.VALUE;
+                }
+                else if (k.name.Equals("userid"))
+                {
+                    UserID = k.VALUE;
+                }
+                else if (k.name.Equals("created"))
+                {
+                    Created = k.VALUE;
+                }
+                else if (k.name.Equals("modified"))
+                {
+                    Modified = k.VALUE;
+                }
+                else if (k.name.Equals("mailed"))
+                {
+                    Mailed = k.VALUE;
+                }
+                else if (k.name.Equals("subject"))
+                {
+                    Subject = k.VALUE;
+                }
+                else if (k.name.Equals("message"))
+                {
+                    Message = k.VALUE;
+                }
+                else if (k.name.Equals("messageformat"))
+                {
+                    MessageFormat = k.VALUE;
+                }
+                else if (k.name.Equals("messagetrust"))
+                {
+                    MessageTrust = k.VALUE;
+                }
+                else if (k.name.Equals("attachment"))
+                {
+                    Attachment = k.VALUE;
+                }
+                else if (k.name.Equals("totalscore"))
+                {
+                    TotalScore = k.VALUE;
+                }
+                else if (k.name.Equals("mailnow"))
+                {
+                    MailNow = k.VALUE;
+                }
+            }
+        }
+
+        private BitmapImage _image;
+        public BitmapImage Image
+        {
+            get
+            {
+                return _image;
+            }
+            set
+            {
+                if (value != _image)
+                {
+                    _image = value;
+                    NotifyPropertyChanged("Image");
+                }
+            }
+        }
+        
+        private string _id;
+        public string ID
+        {
+            get
+            {
+                return _id;
+            }
+            set
+            {
+                if (value != _id)
+                {
+                    _id = value;
+                    NotifyPropertyChanged("ID");
+                }
+            }
+        }
+
+        private string _discussion;
+        public string Discussion
+        {
+            get
+            {
+                return _discussion;
+            }
+            set
+            {
+                if (value != _discussion)
+                {
+                    _discussion = value;
+                    NotifyPropertyChanged("Discussion");
+                }
+            }
+        }
+
+        private string _parent;
+        public string Parent
+        {
+            get
+            {
+                return _parent;
+            }
+            set
+            {
+                if (value != _parent)
+                {
+                    _parent = value;
+                    NotifyPropertyChanged("Parent");
+                }
+            }
+        }
+
+        private string userid;
+        public string UserID
+        {
+            get
+            {
+                return userid;
+            }
+            set
+            {
+                if (value != userid)
+                {
+                    userid = value;
+                    NotifyPropertyChanged("UserID");
+                }
+            }
+        }
+
+        private string _created;
+        public string Created
+        {
+            get
+            {
+                return _created;
+            }
+            set
+            {
+                if (value != _created)
+                {
+                    _created = value;
+                    NotifyPropertyChanged("Created");
+                }
+            }
+        }
+
+        private string _modified;
+        public string Modified
+        {
+            get
+            {
+                return _modified;
+            }
+            set
+            {
+                if (value != _modified)
+                {
+                    _modified = value;
+                    NotifyPropertyChanged("Modified");
+                }
+            }
+        }
+
+        private string _mailed;
+        public string Mailed
+        {
+            get
+            {
+                return _mailed;
+            }
+            set
+            {
+                if (value != _mailed)
+                {
+                    _mailed = value;
+                    NotifyPropertyChanged("Mailed");
+                }
+            }
+        }
+
+        private string _subject;
+        public string Subject
+        {
+            get
+            {
+                return _subject;
+            }
+            set
+            {
+                if (value != _subject)
+                {
+                    _subject = value;
+                    NotifyPropertyChanged("Subject");
+                }
+            }
+        }
+
+        private string _message;
+        public string Message
+        {
+            get
+            {
+                return _message;
+            }
+            set
+            {
+                if (value != _message)
+                {
+                    _message = value;
+                    NotifyPropertyChanged("Message");
+                }
+            }
+        }
+
+        private string _messageFormat;
+        public string MessageFormat
+        {
+            get
+            {
+                return _messageFormat;
+            }
+            set
+            {
+                if (value != _messageFormat)
+                {
+                    _messageFormat = value;
+                    NotifyPropertyChanged("MessageFormat");
+                }
+            }
+        }
+
+        private string _messageTrust;
+        public string MessageTrust
+        {
+            get
+            {
+                return _messageTrust;
+            }
+            set
+            {
+                if (value != _messageTrust)
+                {
+                    _messageTrust = value;
+                    NotifyPropertyChanged("MessageTrust");
+                }
+            }
+        }
+
+        private string _attachment;
+        public string Attachment
+        {
+            get
+            {
+                return _attachment;
+            }
+            set
+            {
+                if (value != _attachment)
+                {
+                    _attachment = value;
+                    NotifyPropertyChanged("Attachment");
+                }
+            }
+        }
+
+        private string _totalScore;
+        public string TotalScore
+        {
+            get
+            {
+                return _totalScore;
+            }
+            set
+            {
+                if (value != _totalScore)
+                {
+                    _totalScore = value;
+                    NotifyPropertyChanged("TotalScore");
+                }
+            }
+        }
+
+        private string _mailnow;
+        public string MailNow
+        {
+            get
+            {
+                return _mailnow;
+            }
+            set
+            {
+                if (value != _mailnow)
+                {
+                    _mailnow = value;
+                    NotifyPropertyChanged("MailNow");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(String propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (null != handler)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
