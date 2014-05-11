9b7dec1fff64b64026dfc6b0cdfbc1951ff9ac19
effendijohanes <johanes.effendi@live.com>
2014-05-11 14:27:26 +0700

    pengumuman akademis
    udah bisa lagi -> pengumuman akademis

--- SCeLEMobile/MainPage.xaml.cs
+++ SCeLEMobile/MainPage.xaml.cs
@@ -15,12 +15,18 @@ using System.Xml.Serialization;
 using SCeLEMobile.Objek;
 using System.IO;
 using System.Collections.ObjectModel;
+using System.Windows.Media.Imaging;
 
 namespace SCeLEMobile
 {
     public partial class MainPage : PhoneApplicationPage
     {
         public ObservableCollection<EnrolledMK> listEnrolledMK = new ObservableCollection<EnrolledMK>();
+
+        ObservableCollection<ThreadMK> mkThreads = new ObservableCollection<ThreadMK>();
+        List<string> usersID = new List<string>();
+        IDictionary<string, User> users = new Dictionary<string, User>();
+
         // Constructor
         public MainPage()
         {
@@ -39,7 +45,71 @@ namespace SCeLEMobile
             {
                 App.ViewModel.LoadData();
             }
-            
+            getThreadsinForum(""+1);
+        }
+
+        private async void getThreadsinForum(string id)
+        {
+            string url = String.Format("{0}/webservice/rest/server.php?wstoken={1}&wsfunction=mod_forum_get_forum_discussions&forumids[]={2}", ParentModel.SceleURL, ParentModel.GetTokenFromIsolatedStorage(), id);
+            Task<string> xmlTask = ParentModel.DownloadXML(new Uri(url));
+            string hasil = await xmlTask;
+            deserialize(hasil);
+        }
+
+        private void deserialize(string hasil)
+        {
+            XmlReader reader = XmlReader.Create(new StringReader(hasil));
+            XmlSerializer mySerializer = new XmlSerializer(typeof(RESPONSE));
+            RESPONSE f = mySerializer.Deserialize(reader) as RESPONSE;
+
+            foreach (SINGLE s in f.multiple.single)
+            {
+                mkThreads.Add(new ThreadMK(s));
+            }
+
+            foreach (ThreadMK tmk in mkThreads)
+            {
+                usersID.Add(tmk.UserID);
+            }
+
+            getUsers();
+
+            lstThread.ItemsSource = mkThreads;
+        }
+
+        private async void getUsers()
+        {
+            string query = String.Format("{0}/webservice/rest/server.php?wstoken={1}&wsfunction=core_user_get_users_by_field&field={2}", ParentModel.SceleURL, ParentModel.GetTokenFromIsolatedStorage(), "id");
+            for (int i = 0; i < usersID.Count; i++)
+            {
+                query = query + "&values[" + i + "]=" + usersID[i];
+            }
+            Task<string> xmlTask = ParentModel.DownloadXML(new Uri(query));
+            string hasil = await xmlTask;
+            deserializeUsers(hasil);
+        }
+
+        private void deserializeUsers(string hasil)
+        {
+            XmlReader reader = XmlReader.Create(new StringReader(hasil));
+            XmlSerializer mySerializer = new XmlSerializer(typeof(RESPONSE));
+            RESPONSE f = mySerializer.Deserialize(reader) as RESPONSE;
+
+            foreach (SINGLE s in f.multiple.single)
+            {
+                User u = new User(s);
+                users[u.ID] = u;
+            }
+
+            foreach (ThreadMK tmk in mkThreads)
+            {
+                tmk.Image = new BitmapImage(new Uri(users[tmk.UserID].ProfileImageURL));
+            }
+        }
+
+        private void list_Thread_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+
         }
 
         private async void userInfoXMLHelper()
