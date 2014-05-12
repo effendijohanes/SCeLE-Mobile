d6e71882a25ceefdba27c76a4e528d0d80bf2785
effendijohanes <johanes.effendi@live.com>
2014-05-13 00:15:53 +0700

    woy

--- SCeLEMobile/MainPage.xaml.cs
+++ SCeLEMobile/MainPage.xaml.cs
@@ -8,7 +8,6 @@ using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
 using SCeLEMobile.Model;
-using RestSharp;
 using System.Threading.Tasks;
 using System.Xml;
 using System.Xml.Serialization;
